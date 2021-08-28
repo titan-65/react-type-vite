d79f0fece74eb077257d3f81fbb6936543ae678a
Vantol Bennett <vvantol2000@icloud.com>
2021-08-28 01:32:53 -0500

    fixes to README and addition modules added

---
+++ README.md
@@ -0,0 +1,2 @@
+# React-Type-Vite
+
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import PurgeIcons from 'vite-plugin-purge-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh(), VitePWA()],
-});
+    plugins: [reactRefresh(), VitePWA(), ],
+});         });
