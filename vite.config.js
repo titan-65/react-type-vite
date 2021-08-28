5ca37f90994d404b4137ade6204b616b94ea80c2
Vantol Bennett <vvantol2000@icloud.com>
2021-08-27 23:35:45 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [reactRefresh(), VitePWA()],
+});
